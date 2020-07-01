a680d1106c8578d5e5663b4073f6a4bf7d22e3f3
UnixLoverSaurabh <saurabh4104@gmail.com>
2020-07-01 14:53:51 +0530

    Updates

---
+++ precache-manifest.417eec51c182e271910307174f013b22.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d92c51abe78beaef45fc2ac1e2f843eb",
+    "url": "/Hotels-WishList/index.html"
+  },
+  {
+    "revision": "b9301bf5fd46feda4506",
+    "url": "/Hotels-WishList/static/css/main.e615e4f2.chunk.css"
+  },
+  {
+    "revision": "7e80fa19e0da4d5d2f5d",
+    "url": "/Hotels-WishList/static/js/2.c41073b0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/Hotels-WishList/static/js/2.c41073b0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b9301bf5fd46feda4506",
+    "url": "/Hotels-WishList/static/js/main.ded0173b.chunk.js"
+  },
+  {
+    "revision": "421fed6bc6145103ad6b",
+    "url": "/Hotels-WishList/static/js/runtime-main.b3f1a4a3.js"
+  }
+]);
